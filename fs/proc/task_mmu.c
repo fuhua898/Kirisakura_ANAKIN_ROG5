6c3220bc7cf2ccc1d79c88f4693f23c716414f2d
fuhua898 <73608169+fuhua898@users.noreply.github.com>
2025-11-23 22:09:26 +0800

    Update task_mmu.c

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -27,17 +27,7 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
-#ifndef BIT_SUS_MAPS
-#define BIT_SUS_MAPS (1 << 0)  // 使用合适的位掩码值
-#endif
 
-#ifndef BIT_SUS_KSTAT  
-#define BIT_SUS_KSTAT (1 << 1) // 使用合适的位掩码值
-#endif
-
-#ifndef susfs_is_current_proc_umounted
-static inline bool susfs_is_current_proc_umounted(void) { return false; }
-#endif
 
 #define SEQ_PUT_DEC(str, val) \
 		seq_put_decimal_ull_width(m, str, (val) << (PAGE_SHIFT-10), 8)
